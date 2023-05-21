9d041015a5f7eb2a4bdc857bac28e61c51beb2e0
Greg <greg19850@gmail.com>
2023-05-21 19:41:31 +0100

    added error handler to request_api_data function

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ def request_api_data(query_char):
     url = 'https://api.pwnedpasswords.com/range/' + query_char
     res = requests.get(url)
 
+    if res.status_code != 200:
+        raise RuntimeError(f"Error: {res.status_code}! Please try again later.")
+    return res
+
