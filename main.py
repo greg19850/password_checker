5e7358db0a22d0519b3d5a14e8ca31a7c05db4c4
Greg <greg19850@gmail.com>
2023-05-22 04:31:08 +0100

    created hashing function, which pass first 5 hash chars to api

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+import hashlib
+
 
 def request_api_data(query_char):
     url = 'https://api.pwnedpasswords.com/range/' + query_char
@@ -8,3 +10,11 @@ def request_api_data(query_char):
         raise RuntimeError(f"Error: {res.status_code}! Please try again later.")
     return res
 
+
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(password.encode('UTF-8')).hexdigest().upper()
+    first_5_char, tail = sha1password[:5], sha1password[5:]
+    request_api_data(first_5_char)
+
+
+pwned_api_check('password123')
